acbf335e740de433fe6a2e86e70063b91307ba50
Yusufhan26 <114617848+Yusufhan26@users.noreply.github.com>
2022-12-18 20:10:34 +0300

    Write a CustomerManager Functions ,call all functions in program.cs and give them parameters

--- CustomerHistory/CustomerManager.cs
+++ CustomerHistory/CustomerManager.cs
@@ -8,6 +8,21 @@ namespace CustomerHistory
 {
     public class CustomerManager
     {
-
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer added " + customer.CustomerName);            
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer deleted " + customer.CustomerName);          
+        }
+        public  void ListCustomer(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Customer "+customer.CustomerName);
+            }
+                                
+        }
     }
 }
--- CustomerHistory/Program.cs
+++ CustomerHistory/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CustomerHistory;
+
+Customer customer1= new Customer();
+customer1.CustomerName = "David";
+customer1.CustomerSurname = "Smith";
+customer1.Nationality = "England";
+customer1.BirthDate = new DateTime(1978,12,2);
+Customer customer2= new Customer();
+customer2.CustomerName = "Ali";
+customer2.CustomerSurname = "Ekinci";
+customer2.Nationality = "Turkey";
+customer2.BirthDate = new DateTime(1995, 7, 25);
+Customer customer3= new Customer();
+customer3.CustomerName = "Jack";
+customer3.CustomerSurname = "Woods";
+customer3.Nationality = "Australia";
+customer3.BirthDate = new DateTime(1965, 6, 3);
+
+CustomerManager manager= new CustomerManager();
+manager.AddCustomer(customer1);
+manager.DeleteCustomer(customer1);
+manager.AddCustomer(customer2);
+manager.DeleteCustomer(customer2);
+manager.AddCustomer(customer3);
+manager.DeleteCustomer(customer3);
+
+Customer[] customers = new Customer[] {customer1,customer2,customer3 };
+manager.ListCustomer(customers);
+
